7e66031f53086c15075b2dd71a6472ede8fc92c3
Moris Kotsch <48104013+KotschM@users.noreply.github.com>
2019-11-12 14:43:18 +0100

    Add files via upload

---
+++ smartbank/smartbank/src/main/java/de/dhbw/parprog/TransferRunner.java
@@ -0,0 +1,21 @@
+package de.dhbw.parprog;
+public class TransferRunner implements Runnable {
+
+    private Bank bank;
+    private Account a, b;
+
+    public TransferRunner(Bank bank, Account a, Account b) {
+        this.a = a;
+        this.b = b;
+        this.bank = bank;
+    }
+
+    @Override
+    public void run() {
+        bank.deposit(a, 500);
+        bank.deposit(b, 500);
+        System.out.println("Account A: " + bank.getBalance(a) + " \nAccount B. " + bank.getBalance(b));
+        bank.transfer(a, b, 200);
+        System.out.println("Account A: " + bank.getBalance(a) + " \nAccount B. " + bank.getBalance(b));
+    }
+}
